750f417377d52df0497e4b1a0b6f6c52585c5310
Ali Syed <alisyed0206@gmail.com>
2025-04-30 08:40:02 -0400

    displaying a list of Taylor Swift songs that I recieved from the iTunes Search API

--- CupcakeCorner/AddressView.swift
+++ CupcakeCorner/AddressView.swift
@@ -7,16 +7,6 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct AddressView: View {
     @Bindable var order: Order
 
--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
     @State private var order = Order()
-
+    @State private var results = [Result]()
+    
     var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+        
         NavigationStack {
             Form {
                 Section {
@@ -40,6 +62,23 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Cupcake Corner")
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid string")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
         }
     }
 }
