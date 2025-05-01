480cedc1b4cca3cb74330c912df737a77bb964aa
Ali Syed <alisyed0206@gmail.com>
2025-05-01 07:47:47 -0400

    loading image from remote server

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -33,6 +33,19 @@ struct ContentView: View {
             await loadData()
         }
         
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading this image.")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200, height: 200)
+        
         NavigationStack {
             Form {
                 Section {
