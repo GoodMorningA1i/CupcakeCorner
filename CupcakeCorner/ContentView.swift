36a3ae128d144f2af3f902db4512802f299c7004
Ali Syed <alisyed0206@gmail.com>
2025-05-11 21:52:39 -0400

    day 1 wrap up, implemented a disabled form

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,91 +7,26 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var order = Order()
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
     
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
-        }
-        
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading this image.")
-            } else {
-                ProgressView()
-            }
-        }
-        .frame(width: 200, height: 200)
-        
-        NavigationStack {
-            Form {
-                Section {
-                    Picker("Select your cake type", selection: $order.type) {
-                        ForEach(Order.types.indices, id: \.self) {
-                            Text(Order.types[$0])
-                        }
-                    }
-
-                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
-                }
-
-                Section {
-                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
-
-                    if order.specialRequestEnabled {
-                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
-
-                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
-                    }
-                }
-
-                Section {
-                    NavigationLink("Delivery details") {
-                        AddressView(order: order)
-                    }
-                }
-            }
-            .navigationTitle("Cupcake Corner")
-        }
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid string")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        } catch {
-            print("Invalid data")
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(disableForm)
         }
     }
 }
