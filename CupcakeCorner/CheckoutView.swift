17ae0eddef71880799a43db833435af2777342e6
Ali Syed <alisyed0206@gmail.com>
2025-09-10 19:52:39 -0400

    day 3 complete, lots of practice with networking concepts

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -51,15 +51,14 @@ struct CheckoutView: View {
             print("Failed to encode order")
             return
         }
-
+        
         let url = URL(string: "https://reqres.in/api/cupcakes")!
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-
+        
         do {
             let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
-
             let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
             confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
             showingConfirmation = true
